03617448adedc558d80669afa23c8e4fb5fe1e14
Levi <jerry.zheng>
2021-02-05 13:53:21 +0800

    :smile:init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <test></test>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Test from './components/test'
+
+export default defineComponent({
+  name: 'App',
+  components:{
+    Test
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
